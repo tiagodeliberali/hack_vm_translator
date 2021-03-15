12c048cbb7a4634a91a6f157d1b4f95dd0e9406b
tiagodeliberali <tiago.santos@quintoandar.com.br>
2021-03-15 19:38:02 -0300

    fixes to pass all tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,20 +1,32 @@
 use rand::prelude::*;
 use std::collections::HashMap;
 
+pub fn initial_data() -> Vec<String> {
+    let mut commands: Vec<String> = Vec::new();
+
+    commands.push(String::from("initialize sp 256"));
+    commands.push(String::from("call Sys.init 0"));
+
+    parse_content(commands, String::from("initialize ASM file"))
+}
+
 pub fn parse_content(content: Vec<String>, filename: String) -> Vec<String> {
     let actions = build_actions();
     let mut result: Vec<String> = Vec::new();
+    let mut instruction = VMInstruction::new(&filename);
+
+    result.push(format!("// New file: {}", &filename));
 
     for line in content {
         result.push(format!("// {}", line));
 
-        let instruction = VMInstruction::new(&line, &filename);
+        &instruction.update(&line);
 
         let action = actions
             .get(&instruction.command)
             .expect("Invalid action required!");
 
-        result.extend(action(instruction));
+        result.extend(action(&instruction));
     }
 
     result
@@ -24,45 +36,85 @@ struct VMInstruction {
     pub command: String,
     pub detail: String,
     pub value: String,
+    pub function_name: String,
     pub filename: String,
 }
 
 impl VMInstruction {
-    pub fn new(line: &String, filename: &str) -> VMInstruction {
-        let (command, detail, value) = VMInstruction::split_command(&line);
-
+    pub fn new(filename: &str) -> VMInstruction {
         VMInstruction {
-            command,
-            detail,
-            value,
+            command: String::new(),
+            detail: String::new(),
+            value: String::new(),
+            function_name: String::new(),
             filename: String::from(filename),
         }
+    }
+
+    pub fn update(&mut self, line: &String) {
+        let (command, detail, value) = VMInstruction::split_command(&line);
+
+        let function_name = if command == "function" {
+            detail.clone()
+        } else {
+            self.function_name.clone()
+        };
+
+        self.command = command;
+        self.detail = detail;
+        self.value = value;
+        self.function_name = function_name;
+    }
+
+    pub fn build_label(&self) -> String {
+        format!("{}${}", self.function_name, self.detail)
+    }
+
+    pub fn build_static_label(&self) -> String {
+        format!("{}${}", self.filename, self.value)
+    }
+
+    pub fn build_label_from(&self, value: &str) -> String {
+        format!("{}${}", self.function_name, value)
+    }
+
+    pub fn build_random_label_from(&self, value: &str) -> String {
+        let random_label = format!("{}", rand::thread_rng().gen::<u32>());
+        format!("{}${}.{}", self.function_name, value, random_label)
+    }
+
+    pub fn build_function_name(&self) -> String {
+        format!("{}", self.detail)
     }
 
     fn split_command(line: &String) -> (String, String, String) {
         let command: Vec<&str> = line.split(' ').collect();
 
         if command.len() == 1 {
-            return (String::from(command[0]), String::new(), String::new());
+            return (
+                String::from(command[0].trim()),
+                String::new(),
+                String::new(),
+            );
         }
 
         if command.len() == 2 {
             return (
-                String::from(command[0]),
-                String::from(command[1]),
+                String::from(command[0].trim()),
+                String::from(command[1].trim()),
                 String::new(),
             );
         }
 
         (
-            String::from(command[0]),
-            String::from(command[1]),
-            String::from(command[2]),
+            String::from(command[0].trim()),
+            String::from(command[1].trim()),
+            String::from(command[2].trim()),
         )
     }
 }
 
-type Callback = fn(VMInstruction) -> Vec<String>;
+type Callback = fn(&VMInstruction) -> Vec<String>;
 
 fn build_actions() -> HashMap<String, Callback> {
     let mut actions: HashMap<String, Callback> = HashMap::new();
@@ -90,10 +142,12 @@ fn build_actions() -> HashMap<String, Callback> {
     actions.insert(String::from("return"), return_action);
     actions.insert(String::from("call"), call_action);
 
+    actions.insert(String::from("initialize"), initialize_action);
+
     actions
 }
 
-fn push_action(instruction: VMInstruction) -> Vec<String> {
+fn push_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     // use value information
@@ -131,7 +185,7 @@ fn push_action(instruction: VMInstruction) -> Vec<String> {
             builder.push_to_stack();
         }
         "static" => {
-            builder.at(format!("{}.{}", instruction.filename, instruction.value).as_str());
+            builder.at(instruction.build_static_label().as_str());
             builder.m_to_d();
             builder.push_to_stack();
         }
@@ -152,47 +206,46 @@ fn push_action(instruction: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn pop_action(instruction: VMInstruction) -> Vec<String> {
+fn pop_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
+    let temp_label = instruction.build_label_from("tmp");
 
     // use value information
     match instruction.detail.as_str() {
         "local" => {
             builder.move_value_to_d(&instruction.value);
             builder.get_address_from_segment_plus_d("LCL");
-            builder.d_to_tmp();
-            builder.pop_from_stack_to("tmp");
+            builder.d_to_tmp(&temp_label);
+            builder.pop_from_stack_to(&temp_label);
         }
         "argument" => {
             builder.move_value_to_d(&instruction.value);
             builder.get_address_from_segment_plus_d("ARG");
-            builder.d_to_tmp();
-            builder.pop_from_stack_to("tmp");
+            builder.d_to_tmp(&temp_label);
+            builder.pop_from_stack_to(&temp_label);
         }
         "this" => {
             builder.move_value_to_d(&instruction.value);
             builder.get_address_from_segment_plus_d("THIS");
-            builder.d_to_tmp();
-            builder.pop_from_stack_to("tmp");
+            builder.d_to_tmp(&temp_label);
+            builder.pop_from_stack_to(&temp_label);
         }
         "that" => {
             builder.move_value_to_d(&instruction.value);
             builder.get_address_from_segment_plus_d("THAT");
-            builder.d_to_tmp();
-            builder.pop_from_stack_to("tmp");
+            builder.d_to_tmp(&temp_label);
+            builder.pop_from_stack_to(&temp_label);
         }
         "temp" => {
             builder.move_value_to_d(&instruction.value);
             builder.at("5");
             builder.d_plus_a_to_d();
-            builder.d_to_tmp();
-            builder.pop_from_stack_to("tmp");
+            builder.d_to_tmp(&temp_label);
+            builder.pop_from_stack_to(&temp_label);
         }
         "static" => {
-            let parsed_value = format!("{}.{}", instruction.filename, instruction.value);
-
             builder.pop_from_stack_to_d();
-            builder.at(parsed_value.as_str());
+            builder.at(instruction.build_static_label().as_str());
             builder.d_to_m();
         }
         "pointer" => {
@@ -212,7 +265,7 @@ fn pop_action(instruction: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn add_action(_: VMInstruction) -> Vec<String> {
+fn add_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
@@ -223,7 +276,7 @@ fn add_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn sub_action(_: VMInstruction) -> Vec<String> {
+fn sub_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
@@ -234,43 +287,43 @@ fn sub_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn eq_action(_: VMInstruction) -> Vec<String> {
+fn eq_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
     builder.pop_from_stack();
     builder.m_less_d_to_d();
-    builder.compare_with_d("JEQ");
+    builder.compare_with_d("JEQ", &instruction.build_random_label_from("IS_TRUE"));
     builder.advance_sp();
 
     builder.parsed_content()
 }
 
-fn lt_action(_: VMInstruction) -> Vec<String> {
+fn lt_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
     builder.pop_from_stack();
     builder.m_less_d_to_d();
-    builder.compare_with_d("JLT");
+    builder.compare_with_d("JLT", &instruction.build_random_label_from("IS_TRUE"));
     builder.advance_sp();
 
     builder.parsed_content()
 }
 
-fn gt_action(_: VMInstruction) -> Vec<String> {
+fn gt_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
     builder.pop_from_stack();
     builder.m_less_d_to_d();
-    builder.compare_with_d("JGT");
+    builder.compare_with_d("JGT", &instruction.build_random_label_from("IS_TRUE"));
     builder.advance_sp();
 
     builder.parsed_content()
 }
 
-fn and_action(_: VMInstruction) -> Vec<String> {
+fn and_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
@@ -281,7 +334,7 @@ fn and_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn or_action(_: VMInstruction) -> Vec<String> {
+fn or_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
@@ -292,7 +345,7 @@ fn or_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn not_action(_: VMInstruction) -> Vec<String> {
+fn not_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack();
@@ -302,7 +355,7 @@ fn not_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn neg_action(_: VMInstruction) -> Vec<String> {
+fn neg_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack();
@@ -312,64 +365,69 @@ fn neg_action(_: VMInstruction) -> Vec<String> {
     builder.parsed_content()
 }
 
-fn label_action(instruction: VMInstruction) -> Vec<String> {
+fn label_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
-    builder.label(&instruction.detail);
+    builder.label(&instruction.build_label());
 
     builder.parsed_content()
 }
 
-fn goto_action(instruction: VMInstruction) -> Vec<String> {
+fn goto_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
-    builder.goto_label(&instruction.detail);
+    builder.goto_label(&instruction.build_label());
 
     builder.parsed_content()
 }
 
-fn ifgoto_action(instruction: VMInstruction) -> Vec<String> {
+fn ifgoto_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
     builder.pop_from_stack_to_d();
-    builder.jump_to_label_if_d_neq(&instruction.detail);
+    builder.jump_to_label_if_d_neq(&instruction.build_label());
 
     builder.parsed_content()
 }
 
-fn function_action(instruction: VMInstruction) -> Vec<String> {
+fn function_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
-    let random_label = format!("{}", rand::thread_rng().gen::<u32>());
+    let while_label = instruction.build_random_label_from("WHILE");
+    let break_label = instruction.build_random_label_from("BREAK");
 
-    builder.label(&instruction.detail);                         // set the label for the function
+    builder.label(&instruction.build_function_name()); // set the label for the function
 
     builder.move_value_to_d(&instruction.value);
     builder.push_to_stack();
 
-    builder.label(format!("WHILE.{}", random_label).as_str()); // while d != 0
+    builder.label(&while_label); // while d != 0
 
     builder.pop_from_stack_to_d();
-    builder.jump_to_label_if_d_eq(&random_label);
+    builder.jump_to_label_if_d_eq(&break_label);
     builder.push_to_stack_zero();
     builder.d_less_one_to_d(); // d--
     builder.push_to_stack();
 
-    builder.goto_label(format!("WHILE.{}", random_label).as_str());
+    builder.goto_label(&while_label);
 
-    builder.label(&random_label); // end while
+    builder.label(&break_label); // end while
 
     builder.parsed_content()
 }
 
-fn return_action(_: VMInstruction) -> Vec<String> {
+fn return_action(_: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
 
+    let temp_return_value = "R13";
+    let temp_return_addr = "R14";
+    let temp_sp = "R15";
+
     builder.pop_from_stack_to_d();
-    builder.at("temp.return.value");
+    builder.at(temp_return_value);
     builder.d_to_m(); // save return value to temp.return.value
 
     builder.get_value_at("ARG");
-    builder.at("temp.sp");
+    builder.at(temp_sp);
     builder.d_to_m(); // save SP original value to temp.sp
 
     builder.get_value_at("LCL");
@@ -393,24 +451,24 @@ fn return_action(_: VMInstruction) -> Vec<String> {
     builder.d_to_m(); // restore LCL value
 
     builder.pop_from_stack_to_d();
-    builder.at("temp.return.addr");
+    builder.at(temp_return_addr);
     builder.d_to_m(); // restore ARG value
 
-    builder.get_value_at("temp.sp");
+    builder.get_value_at(temp_sp);
     builder.at("SP");
     builder.d_to_m(); // restore SP value
 
-    builder.get_value_at("temp.return.value");
+    builder.get_value_at(temp_return_value);
     builder.push_to_stack(); // push return value to stack
 
-    builder.goto_value_at("temp.return.addr"); // go back to flow  
+    builder.goto_value_at(temp_return_addr); // go back to flow
 
     builder.parsed_content()
 }
 
-fn call_action(instruction: VMInstruction) -> Vec<String> {
+fn call_action(instruction: &VMInstruction) -> Vec<String> {
     let mut builder = AssemblerCommandBuilder::new();
-    let random_jump: String = format!("{}", rand::thread_rng().gen::<u32>());
+    let random_jump: String = instruction.build_random_label_from("ret");
 
     builder.label(&random_jump); // get a return point at d
     builder.move_value_to_d(&random_jump);
@@ -435,14 +493,24 @@ fn call_action(instruction: VMInstruction) -> Vec<String> {
     builder.d_plus_a_to_d();
     builder.at("SP");
     builder.m_less_d_to_d();
-    builder.at("ARG"); 
-    builder.d_to_m();       // Move ARGS to first argument
+    builder.at("ARG");
+    builder.d_to_m(); // Move ARGS to first argument
 
     builder.get_value_at("SP");
     builder.at("LCL");
     builder.d_to_m(); // Move LCL to first empty SP (will be filled by function)
 
-    builder.goto_label(&instruction.detail); // go to function
+    builder.goto_label(&instruction.build_function_name()); // go to function
+
+    builder.parsed_content()
+}
+
+fn initialize_action(_: &VMInstruction) -> Vec<String> {
+    let mut builder = AssemblerCommandBuilder::new();
+
+    builder.move_value_to_d("256");
+    builder.at("SP");
+    builder.d_to_m();
 
     builder.parsed_content()
 }
@@ -524,17 +592,14 @@ impl AssemblerCommandBuilder {
         self.result.push(String::from("M=-M"));
     }
 
-    pub fn compare_with_d(&mut self, compare: &str) {
-        let random_jump: u32 = rand::thread_rng().gen();
-        let jump_name = format!("FALSE.{}", random_jump);
-
+    pub fn compare_with_d(&mut self, compare: &str, label_name: &str) {
         self.result.push(String::from("M=-1")); // m = true
-        self.result.push(format!("@{}", jump_name)); // if compare is false, set m to false
+        self.result.push(format!("@{}", label_name)); // if compare is false, set m to false
         self.result.push(format!("D;{}", compare));
         self.result.push(String::from("@SP"));
         self.result.push(String::from("A=M"));
         self.result.push(String::from("M=0"));
-        self.result.push(format!("({})", jump_name)); // end if
+        self.result.push(format!("({})", label_name)); // end if
     }
 
     pub fn m_to_d(&mut self) {
@@ -545,8 +610,8 @@ impl AssemblerCommandBuilder {
         self.result.push(String::from("M=D"));
     }
 
-    pub fn d_to_tmp(&mut self) {
-        self.result.push(String::from("@tmp"));
+    pub fn d_to_tmp(&mut self, label: &str) {
+        self.result.push(format!("@{}", label));
         self.result.push(String::from("M=D"));
     }
 
